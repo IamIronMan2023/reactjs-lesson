8f4a683d1f9e77b860e395d5618271fd03152d9d
jkujo20 <jkujo20@yahoo.com>
2023-03-19 09:13:46 +0800

    Re add Employee and Salary Component

---
+++ lesson/src/components/EmployeeContextComponent.js
@@ -0,0 +1,82 @@
+import AppContext from "./AppContext";
+import React, { useContext } from "react";
+
+const SalaryContextComponent = (props) => {
+  const { monthlySalary } = useContext(AppContext);
+
+  return (
+    <div>
+      <h2>Employee Profile</h2>
+      <p>
+        <label>
+          Employee ID: <b>{props.Id}</b>
+        </label>
+      </p>
+      <p>
+        <label>
+          Employee First Name: <b>{props.firstName}</b>
+        </label>
+      </p>
+      <p>
+        <label>
+          Employee Last Name: <b>{props.lastName}</b>
+        </label>
+      </p>
+      <p>
+        <label>
+          Employee Age: <b>{props.age}</b>
+        </label>
+      </p>
+      <p>
+        <label>
+          Salary:
+          <b>{monthlySalary}</b>
+        </label>
+      </p>
+    </div>
+  );
+};
+
+// class SalaryContextComponent extends React.Component {
+//   static contextType = AppContext;
+
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h2>Employee Profile</h2>
+//         <p>
+//           <label>
+//             Employee ID: <b>{this.props.Id}</b>
+//           </label>
+//         </p>
+//         <p>
+//           <label>
+//             Employee First Name: <b>{this.props.firstName}</b>
+//           </label>
+//         </p>
+//         <p>
+//           <label>
+//             Employee Last Name: <b>{this.props.lastName}</b>
+//           </label>
+//         </p>
+//         <p>
+//           <label>
+//             Employee Age: <b>{this.props.age}</b>
+//           </label>
+//         </p>
+//         <p>
+//           <label>
+//             Salary:
+//             <b>{this.context.monthlySalary}</b>
+//           </label>
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+export default SalaryContextComponent;
