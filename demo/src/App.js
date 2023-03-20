6fbec3b0712f64548defeebcc37c6d11ab5a1eef
jkujo20 <jkujo20@yahoo.com>
2023-03-20 20:49:43 +0800

    Add ClickCounterComponent

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,27 +1,30 @@
 import "./App.css";
+import ClickCounterComponent from "./components/ClickCounterComponent";
 //import CountCharacter from "./components/CharacterCounterComponent";
-import Employee from "./components/EmployeeComponent";
-import SalaryComponent from "./components/SalaryComponent";
-import { SalaryContextProvider } from "./components/SalaryContext";
+// import Employee from "./components/EmployeeComponent";
+// import SalaryComponent from "./components/SalaryComponent";
+// import { SalaryContextProvider } from "./components/SalaryContext";
 //import AddNumberComponent from "./components/AddNumberComponents";
 
 function App() {
   return (
-    <div>
-      <SalaryContextProvider>
-        <Employee
-          Id="007"
-          firstName="James"
-          lastName="Bond"
-          age="20"
-        ></Employee>
-        <SalaryComponent />
-      </SalaryContextProvider>
-    </div>
+    // <div>
+    //   <SalaryContextProvider>
+    //     <Employee
+    //       Id="007"
+    //       firstName="James"
+    //       lastName="Bond"
+    //       age="20"
+    //     ></Employee>
+    //     <SalaryComponent />
+    //   </SalaryContextProvider>
+    // </div>
 
     // <div>
     //   <AddNumberComponent />
     // </div>
+
+    <ClickCounterComponent />
   );
 }
 
