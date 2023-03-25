4c6e19fb248152c7d1e3fd5ed031159c25e6df14
jkujo20 <jkujo20@yahoo.com>
2023-03-25 08:22:58 +0800

    Change CharacterCount to use useRef

--- lesson/src/components/CharacterCounterComponent.jsx
+++ lesson/src/components/CharacterCounterComponent.jsx
@@ -1,37 +1,36 @@
-import React, {useState} from "react";
+import React, { useRef, useState } from "react";
 
 const CharacterCounter = () => {
-    const [data, setData] = useState({
-        characterCount:0 ,
-        textEntered: ""
-    })
+  const dataRef = useRef();
+  const [characterCount, setCharacterCount] = useState(0);
 
-    const onCountCharacters = () => {
-         setData({characterCount: data.textEntered.length});
-    };
+  console.log("render called");
 
-    const handleChanged = (e) => {
-        setData((prev) => {
-            return {...prev, [e.target.name]: e.target.value}
-        })
-    };
+  const onCountCharacters = () => {
+    setCharacterCount(dataRef.current.value.length);
+  };
 
-    return (
-        <div>
-        <h2>Character Counter</h2>
-        <p>
-            <label>Enter a text </label>
-            <input type="text" name="textEntered" onChange={handleChanged}></input>
-        </p>
-        <p>
-            <label>
-            The text has {data.characterCount} number of characters
-            </label>
-        </p>
-        <p>
-            <button onClick={onCountCharacters}>Count</button>
-        </p>
-        </div>
-    );
-}
-export default CharacterCounter
+  //   const handleChanged = (e) => {
+  //     setData((prev) => {
+  //       return { ...prev, [e.target.name]: e.target.value };
+  //     });
+  //   };
+
+  return (
+    <div>
+      <h2>Character Counter</h2>
+      <p>
+        <label>Enter a text </label>
+        {/* <input type="text" name="textEntered" onChange={handleChanged}></input> */}
+        <input type="text" ref={dataRef}></input>
+      </p>
+      <p>
+        <label>The text has {characterCount} number of characters</label>
+      </p>
+      <p>
+        <button onClick={onCountCharacters}>Count</button>
+      </p>
+    </div>
+  );
+};
+export default CharacterCounter;
