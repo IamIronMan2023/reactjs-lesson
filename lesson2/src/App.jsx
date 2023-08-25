c2e7f6827a25051c70a1dbe95aa219b4e06a7922
IamIronMan2023 <kramsotnas@yahoo.com>
2023-08-25 17:00:06 +0800

    Remove App.css and replace with index.css

--- lesson2/src/App.jsx
+++ lesson2/src/App.jsx
@@ -1,16 +1,22 @@
+import ClickCountComponent from "./components/ClickCountComponent";
 import Employee from "./components/EmployeeFunctionComponent";
+import EventComponent from "./components/EventComponent";
 
 function App() {
   return (
     <>
-      <Employee
+      {/* <Employee
         id="007"
         firstName="James"
         lastName="Bond"
         age="20"
         departmentId="1"
         departmentName="CIA"
-      />
+      /> */}
+
+      <EventComponent />
+
+      {/* <ClickCountComponent /> */}
     </>
   );
 }
