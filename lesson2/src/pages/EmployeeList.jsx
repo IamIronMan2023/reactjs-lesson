26e66daed2a1fe8c57b2da0f39eb4ca112ba92f2
IamIronMan2023 <kramsotnas@yahoo.com>
2023-08-26 08:56:52 +0800

    Add EmployeeList and EmployeeView

---
+++ lesson2/src/pages/EmployeeList.jsx
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const EmployeeList = () => {
+  const [employees, setEmployees] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    const requestOptions = {
+      signal: controller.signal,
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      "Access-Control-Allow-Origin": true,
+    };
+
+    setLoading(true);
+    fetch("http://127.0.0.1:8000/api/employees", requestOptions)
+      .then((response) => response.json())
+      .then((json) => {
+        setEmployees(json);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
+  return (
+    <>
+      <h2>Employee List</h2>
+      {loading ? (
+        <h3>Loading...</h3>
+      ) : (
+        <>
+          <table>
+            <thead>
+              <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {employees.map((employee) => (
+                <tr key={employee.id}>
+                  <td>{employee.first_name}</td>
+                  <td>{employee.last_name}</td>
+                  <td>{employee.email}</td>
+                  <td>
+                    <Link to={`/employees/${employee.id}`}>View</Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </>
+      )}
+    </>
+  );
+};
+
+export default EmployeeList;
