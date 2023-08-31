8f5a37a4ebbbcaa58fda5bca8dccb7295e0f3924
IamIronMan2023 <kramsotnas@yahoo.com>
2023-08-31 18:39:46 +0800

    modify style using refs

--- lesson2/src/components/UseRefComponent.jsx
+++ lesson2/src/components/UseRefComponent.jsx
@@ -13,21 +13,33 @@ const UseRefComponent = () => {
     );
   };
 
+  const handleChangeRef = (e) => {
+    // console.log(firstNameRef.current);
+    // firstNameRef.current.style.backgroundColor = "blue";
+    // firstNameRef.current.style.color = "blue";
+    // firstNameRef.current.style.fontWeight = "bold";
+    // firstNameRef.current.focus();
+  };
+
   return (
     <div>
       <h2>Use Ref App</h2>
       <p>
         <label>First Name </label>
-        <input type="text" ref={firstNameRef} />
+        <input type="text" name="firstName" ref={firstNameRef} />
       </p>
       <p>
         <label>Last Name </label>
-        <input type="text" ref={lastNameRef} />
+        <input type="text" name="lastName" ref={lastNameRef} />
       </p>
 
       <p>
         <button onClick={handleClick}>Click</button>
       </p>
+      <p>
+        <button onClick={handleChangeRef}>Change Ref</button>
+      </p>
+
       <p>
         <label>{message}</label>
       </p>
